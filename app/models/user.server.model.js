b9e2d4b90c8e8384352711175a439f68efc2dfd7
Desert-Dragen (Natalie) <52254100+DesertDragen@users.noreply.github.com>
2020-04-15 17:55:39 -0400

    user model

---
+++ app/models/user.server.model.js
@@ -0,0 +1,15 @@
+﻿// Loads the mongoose module and Schema object.
+const mongoose = require('mongoose');
+
+// Defines the Customer schema.
+const CustomerSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    accountType: {
+        type: String,
+        enum: ["Patient", "Nurse"]
+    }
+});
+
+// Creates the customer model.
+mongoose.model('Customer', CustomerSchema);
