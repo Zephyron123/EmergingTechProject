ee5b6494f050a8ac04e86a368353f71e99eae7c9
Desert-Dragen (Natalie) <52254100+DesertDragen@users.noreply.github.com>
2020-04-15 17:53:24 -0400

    signup & login page

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,10 +1,12 @@
 ﻿module.exports = (app) => {
     // Sets the controllers
 	const index = require('../controllers/index.server.controller');
-	const login = require('../controllers/login.server.controller');
+    const login = require('../controllers/login.server.controller');
+    const signup = require('../controllers/signup.server.controller');
 
     // Route the default route.
     app.get('/', index.render);
 	app.post('/', index.render);
-	app.get('/login', login.render);
+    app.get('/login', login.render);
+    app.get('/signup', signup.render);
 }
