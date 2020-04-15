1d805b7764a2ea767b9fe320551dc26dfca90f54
FlubNub <eeyo@rogers.com>
2020-04-15 17:22:39 -0400

    login

---
+++ app/controllers/login.server.controller.js
@@ -0,0 +1,24 @@
+const strUtils = require('../utils/string.server.utils');
+const customer = require('./customer.server.controller');
+
+
+exports.render = (req, res, next) => {
+    /*if (req.method == "POST") {
+        const body = req.body;
+
+        if (body.email && body.password) {
+            // All the fields are filled. Perform the login
+            customer.login(req, res, next);
+        } else {
+            // Missing fields. Re-render the page passing an error message.
+            res.render('index', {
+                error: 'Please, enter your email and password!',
+                email: strUtils.getSafe(body.email),
+            });
+        }
+    } else {*/
+        res.render('login', {
+            title: "Main Page"
+        });
+    //}
+}
