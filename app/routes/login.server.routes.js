d3b620b327451e9c2acc63e8b8f903f5500cdf71
Zephyron123 <46720554+Zephyron123@users.noreply.github.com>
2020-04-15 17:27:54 -0400

    lol

---
+++ app/routes/login.server.routes.js
@@ -0,0 +1,8 @@
+﻿module.exports = (app) => {
+    // Sets the controllers
+    const login = require('../controllers/login.server.controller');
+
+    // Route the default route.
+    app.get('/', login.render);
+    app.post('/', login.render);
+}
