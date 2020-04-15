d09bdb20e6548bea4c41d9d51288ff4fda9f959f
Nalturan <XxnukeraiderxX@gmail.com>
2020-04-15 17:06:39 -0400

    update 1.0

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -2,24 +2,23 @@
 const customer = require('./customer.server.controller');
 
 
-exports.render = (req, res, next) => {
-    if (req.method == "POST") {
-        const body = req.body;
+ exports.render = (req, res, next) => {
+//     if (req.method == "POST") {
+//         const body = req.body;
 
-        if (body.email && body.password) {
-            // All the fields are filled. Perform the login
-            customer.login(req, res, next);
-        } else {
-            // Missing fields. Re-render the page passing an error message.
-            res.render('index', {
-                error: 'Please, enter your email and password!',
-                email: strUtils.getSafe(body.email),
-            });
-        }
-    } else {
+//         if (body.email && body.password) {
+//             // All the fields are filled. Perform the login
+//             customer.login(req, res, next);
+//         } else {
+//             // Missing fields. Re-render the page passing an error message.
+//             res.render('index', {
+//                 error: 'Please, enter your email and password!',
+//                 email: strUtils.getSafe(body.email),
+//             });
+//         }
+//     } else {
         res.render('index', {
-            error: '',
-            email: ''
+            title: "Metro General Home Page"
         });
-    }
+//     }
 }
